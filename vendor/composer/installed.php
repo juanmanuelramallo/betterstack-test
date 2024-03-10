71b1848635e5f3e5f151373e3b87a86da30832f3
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2024-03-10 01:30:12 -0300

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cdb6d0a49af658ae521acce1f76bbec3a3b1a8e9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cdb6d0a49af658ae521acce1f76bbec3a3b1a8e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
