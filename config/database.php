13f50135d3e24adb966201f2eabe1a52a85567e1
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2024-03-10 01:57:46 -0300

    Added .env locally

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 $database = array(
-  'address'   => $_ENV['DB_ADDRESS'] || 'localhost',
-  'username'  => $_ENV['DB_USERNAME'] || 'root',
-  'password'  => $_ENV['DB_PASSWORD'] || '',
-  'database'  => $_ENV['DB_DATABASE'] || 'test_project_main'
+  'address'   => getenv('DB_ADDRESS'),
+  'username'  => getenv('DB_USERNAME'),
+  'password'  => getenv('DB_PASSWORD'),
+  'database'  => getenv('DB_DATABASE')
 );
